c1b70c91a41ef2f475db2d280f7faf890f757032
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-09 15:01:00 -0300

    Atualizando o evento

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import ListaDeEventos from "./components/ListaDeEventos";
 import { RecoilRoot } from "recoil";
 
 function App() {
-/*   const [eventos, setEventos] = useState<IEvento[]>([
+  /*   const [eventos, setEventos] = useState<IEvento[]>([
     {
       descricao: "Estudar React",
       inicio: new Date("2022-01-15T09:00"),
@@ -29,27 +29,27 @@ function App() {
 
   const adicionarEvento = (evento: IEvento) => {
     evento.id = Math.round(new Date().getTime() / 1000);
-/*     eventos.push(evento);
+    /*     eventos.push(evento);
     console.log(eventos);
 
     setEventos([...eventos]); */
   };
-  const alterarStatusEvento = (id: number) => {
-/*     const evento = eventos.find((evento) => evento.id === id);
+  /*   const alterarStatusEvento = (id: number) => {
+     const evento = eventos.find((evento) => evento.id === id);
     if (evento) {
       evento.completo = !evento.completo;
     }
-    setEventos([...eventos]); */
-  };
-  const deletarEvento = (id: number) => {
-    /* setEventos([...eventos.filter((evento) => evento.id !== id)]); */
-  };
+    setEventos([...eventos]); 
+  }; */
+  /*   const deletarEvento = (id: number) => {
+     setEventos([...eventos.filter((evento) => evento.id !== id)]); 
+  }; */
 
   const aplicarFiltro = (data: Date | null) => {
     setFiltro(data);
   };
 
-/*   const filtrados = !filtro
+  /*   const filtrados = !filtro
     ? eventos
     : eventos.filter(
         (evento) =>
@@ -66,14 +66,11 @@ function App() {
           </Card>
           <hr />
           <Card>
-            <ListaDeEventos
-              aoFiltroAplicado={aplicarFiltro}
-              aoAlterarStatus={alterarStatusEvento}
-            />
+            <ListaDeEventos aoFiltroAplicado={aplicarFiltro} />
           </Card>
         </div>
         <div className={style.Coluna}>
-          <Calendario/>
+          <Calendario />
         </div>
       </div>
     </RecoilRoot>
