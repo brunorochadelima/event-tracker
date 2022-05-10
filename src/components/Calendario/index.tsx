c1f9a9d35d87523f0b2e1a4bbe7e97f9c481a9bb
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-10 09:28:06 -0300

    refatorando código duplicado

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -4,8 +4,8 @@ import ptBR from "./localizacao/ptBR.json";
 import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from "kalend";
 import "kalend/dist/styles/index.css";
 import { listaDeEventosState } from "../../State/atom";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { IEvento } from "../../interfaces/IEvento";
+import { useRecoilValue } from "recoil";
+import useAtualizarEvento from "../../State/hooks/useAtualizarEvento";
 
 interface IKalendEvento {
   id?: number;
@@ -18,7 +18,7 @@ interface IKalendEvento {
 const Calendario: React.FC = () => {
   const eventosKalend = new Map<string, IKalendEvento[]>();
   const eventos = useRecoilValue(listaDeEventosState);
-  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+  const atualizarEvento = useAtualizarEvento();
 
   eventos.forEach((evento) => {
     const chave = evento.inicio.toISOString().slice(0, 10);
@@ -47,15 +47,7 @@ const Calendario: React.FC = () => {
       };
       eventoAtualizado.inicio = new Date(KalendEventoAtualizado.startAt);
       eventoAtualizado.fim = new Date(KalendEventoAtualizado.endAt);
-
-      setListaDeEventos((listaAntiga) => {
-        const indice = listaAntiga.findIndex((evt) => evt.id === evento.id);
-        return [
-          ...listaAntiga.slice(0, indice),
-          eventoAtualizado,
-          ...listaAntiga.slice(indice + 1),
-        ];
-      });
+      atualizarEvento(eventoAtualizado);
     }
   };
 
